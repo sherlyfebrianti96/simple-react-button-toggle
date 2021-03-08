ffac8358d39039c06adacfc7fec2e304d883a47d
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-03-08 16:25:29 +0100

    git counter final result

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  // change code below this line
+
+  increment() {
+    console.log("this.state.count : ", this.state.count);
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  reset() {
+    this.setState({
+      count: 0
+    });
+  }
+
+  // change code above this line
+  render() {
+    return (
+      <div>
+        <button className="inc" onClick={this.increment.bind(this)}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement.bind(this)}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset.bind(this)}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
